9d5852114a58844adc7a6ad3da4124e6c9dbb6c4
Victor Manuel Cruz Reyes <vcruzrey1@gmail.com>
2019-11-08 15:45:04 -0600

    Firts PUSH
    Definiciones en YACC

---
+++ calc.py
@@ -0,0 +1,175 @@
+# ------------------------------------------------------------
+# calclex.py
+#
+# tokenizer for a simple expression evaluator for
+# numbers and +,-,*,/
+# ------------------------------------------------------------
+import ply.lex as lex
+
+# List of token names.   This is always required
+
+tokens = [
+    'ID',
+
+    #Data Types
+    'DTI', #int
+    'DTF', #float
+    'DTB', #bool
+    'DTS', #string
+
+    #Arithmetic Operators
+    'EQUALS',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'POWER',
+    'MODULE',
+
+    #Logical Operators
+    'AND',
+    'OR',
+    'NOT',
+
+    #Relational Operators
+    'GREATER',
+    'LOWER',
+    'SAME',
+    'LEQUAL',
+    'GEQUAL',
+    'NOTEQUAL',
+
+    #Structure
+    'LPAREN',
+    'RPAREN',
+    'LBRCKT',
+    'RBRCKT',
+    'LCORCHO',
+    'RCORCHO',
+    'COMMA',
+    'PNTCOMMA',
+    'COLON',
+    'POINT',
+ ]
+
+reserved = {
+
+    #Data Type
+    'int' : 'INT',
+    'float' : 'FLOAT',
+    'bool' : 'BOOL',
+    'string' : 'STRING',
+    'cons' : 'CONS',
+    'false' : 'FALSE',
+    'true' : 'TRUE',
+
+    #Data Structures
+    'arr' : 'ARR',
+    'mat' : 'MAT',
+
+    #Structure
+    'main' : 'MAIN',
+    'func' : 'FUNC',
+
+    #Print
+    'print' : 'PRINT',
+
+    #Conditions
+    'if' : 'IF',
+    'else' : 'ELSE',
+    'switch': 'SWITCH',
+    'case' : 'CASE',
+    'break' : 'BREAK',
+
+    #Loops
+    'while' : 'WHILE',
+    'do' : 'DO',
+    'for' : 'FOR',
+
+    #Math Functions
+    'sqrt' : 'SQRT',
+    'mode' : 'MODE',
+    'mean' : 'MEAN',
+    'avg' : 'AVERAGE',
+    'pi' : 'PI',
+
+    'end' : 'END',
+}
+
+tokens += list(reserved.values())
+
+# Regular expression rules for simple tokens
+t_PLUS    = r'\+'
+t_EQUALS  = r'\='
+t_MINUS   = r'\-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_POWER   = r'\^'
+t_RBRCKT  = r'\]'
+t_LBRCKT  = r'\['
+t_COMMA   = r'\,'
+t_PNTCOMMA  = r'\;'
+t_DTS = r'\".*?\"'
+t_AND = r'\&&'
+t_OR = r'[|][|]'
+t_LCORCHO = r'\{'
+t_RCORCHO = r'\}'
+t_COLON = r'\:'
+t_POINT = r'\.'
+t_GREATER = r'\>'
+t_LOWER = r'\<'
+t_SAME = r'\=\='
+t_LEQUAL = r'\<\='
+t_GEQUAL = r'\>\='
+t_NOTEQUAL = r'\!\='
+t_NOT = r'\!'
+t_MODULE = r'\%'
+
+
+# A regular expression rule with some action code
+def t_DTF(t):
+    r'(\d*\.\d+)|(\d+\.\d*)'
+    t.value = float(t.value)
+    return t
+
+#Integer definition
+def t_DTI(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+
+#bool def
+def t_DTB(t):
+    r'(true|false)'
+    mapping = {"true": True, "false": False}
+    t.value = mapping[t.value]
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+     r'\n+'
+     t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+     print("Illegal character '%s'" % t.value[0])
+     t.lexer.skip(1)
+
+def t_ID(t):
+     r'[a-zA-Z_][a-zA-Z_0-9]*'
+     t.type = reserved.get(t.value,'ID')    # Check for reserved words
+     return t
+
+
+#def t_Comment(t):
+#	r'[/][*][^*]*[*]+([^*/][^*]*[*]+)*[/] | [/][/].*'
+#	pass
+
+# Build the lexer
+lexer = lex.lex()
---
+++ yacc.py
@@ -0,0 +1,207 @@
+import sys
+import ply.yacc as yacc
+from calc import tokens
+
+# PROGRAMA
+def p_programa(p):
+    '''
+    programa : globales principal END
+    '''
+    p[0] = "PROGRAM COMPILED"
+
+#Variables globales
+#Existen: 0 o mas
+def p_globales(p):
+    '''
+    globales : declarar globales
+             | empty
+    '''
+
+#Funciones
+#Existen: 0 o mas
+def p_funciones(p):
+    '''
+    funciones : metodos funciones
+              | empty
+    '''
+
+#Principal
+#Existen: 1
+def p_principal(p):
+    '''
+    principal : LCORCHE principalloop RCORCHE
+    '''
+
+def p_principalloop(p):
+    '''
+    principalloop : estatuto principalloop
+                  | estatuto
+                  | empty
+    '''
+
+#ESTATUTO
+def p_estatuto(p):
+    '''
+    estatuto : declarar
+             | asignacion
+             | asignacionarr
+             | condicion
+             | escritura
+    '''
+
+def p_declarar(p):
+    '''
+    declarar : inicializada
+             | CONS inicializada
+             | noinicializada
+    '''
+
+def p_inicializada(p):
+    '''
+    inicializada : tipo asignacion
+                 | ARR tipo asignacioninicialarr
+                 | MAT tipo asignacioninicialmat
+    '''
+
+def p_noinicializada(p):
+    '''
+    noinicializada : tipo ID PNTCOMMA
+                   | ARR tipo ID bracket PNTCOMMA
+                   | MAT tipo ID bracket bracket PNTCOMMA
+    '''
+
+def p_bracket(p):
+    '''
+    bracket : LBRCKT DTI RBRCKT
+            | LBRCKT ID RBRCKT
+    '''
+
+# TIPO
+def p_tipo(p):
+    '''
+    tipo : INT
+         | FLOAT
+         | BOOL
+         | STRING
+    '''
+
+# asignacion
+def p_asignacion(p):
+    '''
+    asignacion : ID EQUALS expresion PNTCOMMA
+    '''
+
+def p_asignacionarr(p):
+    '''
+    asignacionarr : ID bracket EQUALS expresion PNTCOMMA
+    '''
+
+def p_asignacioninicialarr(p):
+    '''
+    asignacioninicialarr : ID bracket EQUALS looparreglo PNTCOMMA
+    '''
+
+def p_asignacioninicialmat(p):
+    '''
+    asignacioninicialmat : ID bracket bracket EQUALS looparreglo looparreglo PNTCOMMA
+    '''
+
+def p_looparreglo(p):
+    '''
+    looparreglo : LBRCKT expresion expresionloop RBRCKT
+    '''
+
+def p_expresionloop(p):
+    '''
+    expresionloop : COMMA expresion expresionloop
+                  | empty
+    '''
+    
+#Expresion
+def p_expresion(p):
+    '''
+    expresion : exp expresionrelacional
+    '''
+
+def p_expresionrelacional(p):
+    '''
+    expresionrelacional : GREATER exp
+                        | LOWER exp
+                        | SAME exp
+                        | LEQUAL exp
+                        | GEQUAL exp
+                        | NOTEQUAL exp
+                        | AND exp
+                        | OR exp
+                        | empty
+    '''
+
+# EXP
+def p_exp(p):
+    '''
+    exp : termino expsumres
+    '''
+
+def p_expsumres(p):
+    '''
+    expsumres : PLUS termino expsumres
+              | MINUS termino expsumres
+              | empty
+    '''
+
+#Termino
+def p_termino(p):
+    '''
+    termino : factor terminomuldiv
+    '''
+
+def p_terminomuldiv(p):
+    '''
+    terminomuldiv : TIMES factor terminomuldiv
+                  | DIVIDE factor terminomuldiv
+                  | empty
+    '''
+
+#Factor
+def p_factor(p):
+    '''
+    factor : LPAREN expresion RPAREN
+           | PLUS vardt
+           | MINUS vardt
+           | vardt
+    '''
+
+#VARDT
+def p_vardt(p):
+    '''
+    vardt : ID
+          | DTI
+          | DTF
+          | DTB
+          | DTS
+          | ID bracket
+          | ID bracket bracket
+    '''
+
+def p_empty(p):
+    '''empty :'''
+
+def p_error(p):
+    print("ERROR {}".format(p))
+
+yacc.yacc()
+
+if __name__ == '__main__':
+    try:
+        arch_name = 'prueba-1.txt'
+        arch = open(arch_name,'r')
+        print("Leyendo archivo: " + arch_name + "...")
+        info = arch.read()
+        # print(info)
+        arch.close()
+        if(yacc.parse(info, tracking=True) == 'PROGRAM COMPILED'):
+            print("SINTAXIS VÁLIDA")
+        else:
+            print("ERRORES EN LA SINTAXIS")
+    except EOFError:
+        print(EOFError)
