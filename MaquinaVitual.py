9a06eb9b3f9a29454d9fa58b03e0945145bbea03
Rolando Mata <rdma95@gmail.com>
2019-11-23 20:37:32 -0600

    cambios quads

--- MaquinaVitual.py
+++ MaquinaVitual.py
@@ -5,33 +5,41 @@ import sys
 import json
 
 aux_memoria = Memoria()
-matematicas = ['+', '*', '-', '/', '&&','<']
+matematicas = ['+', '-', '*', '/','^','==', '!=', '>', '<', '>=', '<=','&&', '||','%','!']
 #Globales 10000
 #Constantes 20000
 #Locales 30000
 
 #Int 0 - 999
-#FLoat
-#String 
-#Bool
+#FLoat 1000- 1999
+#String 2000-2999
+#Bool 3000-3999
 print(aux_memoria)
 class MaquinaVirtual:
     def __init__ (self):
         self.quadruplesList = []
         self.posicion = 0
         #Quadruplos desde Main
-        self.quadruplesList.append(['=',20000,None,10000])
-        self.quadruplesList.append(['=',20001,None,10001])
-        self.quadruplesList.append(['+',10000,10001,30000])
-        self.quadruplesList.append(['=',30000,None,10002])
-        #print 30000
-        #a = 5
-        #b = 7
-        #c  = a + b
+        self.quadruplesList.append(['=',20000,None,30000])
+        self.quadruplesList.append(['=',21000,None,31000])
+        self.quadruplesList.append(['<',20002,20000,33000])
+        self.quadruplesList.append(['=',33000,None,33001])
+        self.quadruplesList.append(['>',30000,20003,33002])
+        self.quadruplesList.append(['gotof',33002,None,10])#not sure si jala
+        self.quadruplesList.append(['^',31000,30000,31001])
+        self.quadruplesList.append(['print', 31001, None, None])
+        self.quadruplesList.append(['-',30000,20004,30000])
+        self.quadruplesList.append(['goto', None, None,5])
+        self.quadruplesList.append(['print',31000, None, None])
+        self.quadruplesList.append(['print',30000, None, None])
 
         #Escribir Constantes
-        aux_memoria.diccionario['constante'][20000] = "hola"
-        aux_memoria.diccionario['constante'][20001] = "  jesus"
+        aux_memoria.diccionario['constante'][20000] = 3
+        aux_memoria.diccionario['constante'][21000] = 6.0
+        aux_memoria.diccionario['constante'][20002] = 5
+        aux_memoria.diccionario['constante'][20003] = 0
+        aux_memoria.diccionario['constante'][20004] = 1
+
 
         #Ejemplos Escritura
         #aux_memoria.diccionario['global'][5000] = int(4)
@@ -79,10 +87,20 @@ class MaquinaVirtual:
                 "*" : operator.mul,
                 "/" : operator.truediv,
                 "&&" : operator.and_,
-                "<" : operator.lt
+                "|" :  operator.or_,
+                "<" : operator.lt,
+                ">" : operator.gt,
+                "%" : operator.mod,
+                "!" : operator.not_,
+                "^" : operator.pow,
+                ">=" : operator.ge,
+                "<=" : operator.le,
+                "==" : operator.eq,
+                "!=" : operator.ne,
+
             }
             func_op = operations_switch[operator2]
-            print(func_op)
+            print(func_op)   
             res = func_op(self.get_value(mem_izq, izquierda), self.get_value(mem_der, derecha))
             self.set_value(mem_res, resultado, res)
             self.posicion += 1
@@ -93,76 +111,36 @@ class MaquinaVirtual:
             self.set_value(mem_res, resultado, res)
             self.posicion += 1            
 
-        elif (operator == '%'):
-            res = self.get_value(mem_izq, izquierda) % self.get_value(mem_der, derecha)
-            self.set_value(mem_res, resultado, res)
-            self.posicion += 1
-
-        elif (operator == '^'):
-            res = self.get_value(mem_izq, izquierda) ** self.get_value(mem_der, derecha)
-            self.set_value(mem_res, resultado, res)
-            self.posicion += 1
-
-        elif(operator == '&&'):
-            res = self.get_value(mem_izq, izquierda) and self.get_value(mem_der, derecha)
-            self.set_value(mem_res, resultado, res)
-            self.posicion += 1
-
-        elif (operator == '|'):
-            res = self.get_value(mem_izq, izquierda) + self.get_value(mem_der, derecha)
-            self.set_value(mem_res, resultado, res)
-            self.posicion += 1
         
-        elif (operator == '>'):
-            print("greather")
-            resultado = izquierda > derecha
-            posicion += 1
-
-        elif (operator == '<'):
-            print ("lower")
-            resultado = izquierda < derecha
-            posicion += 1
-
-        elif (operator == '=='):
-            print ("equal")
-            resultado = izquierda == derecha
-            posicion += 1
-        
-        elif (operator == '<='):
-            print("loweq")
-            resultado = izquierda <= derecha
-            posicion += 1
-        
-        elif (operator == '>='):
-            print("greq")
-            resultado = izquierda >= derecha
-            posicion +=1
-
-        elif (operator == '!='):
-            print("noteq")
-            resultado = izquierda != derecha
-            posicion +=1
-
         #CHECAR QUE JALE ESTE EN ESPECIFICO
-        elif (operator == '!'):
-            print("not")
-            resultado = not derecha
-            posicion += 1
+       # elif (operator == '!'):
+        #    print("not")   
+          #  posicion += 1
+         #   resultado = not derecha
 
         elif (operator == 'print'):
             print("print")
-            print(resultado)
-            posicion += 1
+            res  = self.get_value(mem_res, resultado)
+            print(res)
+            self.posicion += 1
 
         #FALTAN POR DEFINIR
         elif (operator == 'gotof'):
             print("gotof")
+            res  = self.get_value(mem_izq, izquierda)
+            if (res):
+                self.posicion += 1
+                print("here")
+            else:
+                self.posicion = resultado
 
         elif (operator == 'gotov'):
             print('gotov')
+            self.posicion = resultado
 
         elif (operator == 'goto'):
             print('goto')
+            self.posicion = resultado
 
         elif (operator == 'GOSUB'):
             print("gosub")
@@ -184,6 +162,14 @@ class MaquinaVirtual:
             return 'local'
 
 test = MaquinaVirtual()
+test.funciones()
+test.funciones()
+test.funciones()
+test.funciones()
+test.funciones()
+test.funciones()
+test.funciones()
+test.funciones()
 test.funciones()
 test.funciones()
 test.funciones()
